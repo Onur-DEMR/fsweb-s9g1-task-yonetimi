7f134cbe67c2cba458a7436a65915da8bb026ad0
Onur-DEMR <119036165+Onur-DEMR@users.noreply.github.com>
2023-02-05 15:11:35 +0300

    Task hook form tasarlanmaya başlandı.

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function TaskHookForm() {
+  const { register, handleSubmit } = useForm();
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <form className="taskForm" onSubmit={handleSubmit}>
+      <div className="form-line">
+        <label className="input-label" htmlFor="title">
+          Başlık
+        </label>
+        <input className="input-text" id="title" name="title" type="text" />
+      </div>
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+        </label>
+        <textarea
+          className="input-textarea"
+          rows="3"
+          id="description"
+          name="description"></textarea>
+      </div>
+      <div className="form-line">
+        <label className="input-label">İnsanlar</label>
+        <label className="input-checkbox">
+          <input type="checkbox" name="people" value="Onur" />
+          Onur
+        </label>
+      </div>
+      <div className="form-line">
+        <button
+          className="submit-button"
+          type="submit"
+          // disabled={}
+        >
+          Kaydet
+        </button>
+      </div>
+    </form>
+  );
 }
