4a9828f88de0827c72f3635f1cfc6e3038270e8a
Onur-DEMR <119036165+Onur-DEMR@users.noreply.github.com>
2023-02-05 16:30:21 +0300

    Task hookta rhf düzenlendi.

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -2,14 +2,35 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function TaskHookForm() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
+  function newSubmit(data) {
+    console.log(data);
+  }
   return (
-    <form className="taskForm" onSubmit={handleSubmit}>
+    <form className="taskForm" onSubmit={handleSubmit(newSubmit)}>
       <div className="form-line">
         <label className="input-label" htmlFor="title">
           Başlık
         </label>
-        <input className="input-text" id="title" name="title" type="text" />
+        <input
+          className="input-text"
+          id="title"
+          {...register("title", {
+            required: "Task başlığı yazmalısınız",
+            minLength: {
+              value: 3,
+              message: "Task başlığı en az 3 karakter olmalı",
+            },
+          })}
+          type="text"
+        />
+        <p className="input-error">
+          {errors.title?.required || errors.title?.message}
+        </p>
       </div>
       <div className="form-line">
         <label className="input-label" htmlFor="description">
@@ -19,21 +40,47 @@ export default function TaskHookForm() {
           className="input-textarea"
           rows="3"
           id="description"
-          name="description"></textarea>
+          {...register("description", {
+            required: "Task açıklaması yazmalısınız",
+            minLength: {
+              value: 10,
+              message: "Task açıklaması en az 10 karakter olmalı",
+            },
+          })}></textarea>
+        <p className="input-error">{errors.description?.required}</p>
       </div>
       <div className="form-line">
         <label className="input-label">İnsanlar</label>
         <label className="input-checkbox">
-          <input type="checkbox" name="people" value="Onur" />
+          <input
+            type="checkbox"
+            {...register("people", {
+              required: "Lütfen en az bir kişi seçin",
+              minLength: {
+                value: 1,
+                message: "Lütfen en az bir kişi seçin",
+              },
+              maxLength: {
+                value: 3,
+                message: "En fazla 3 kişi seçebilirsiniz",
+              },
+            })}
+            value="Onur"
+          />
+          Onur
+        </label>
+        <label className="input-checkbox">
+          <input
+            type="checkbox"
+            {...register("people", { required: true })}
+            value="Gülşah"
+          />
           Onur
         </label>
       </div>
+      <p className="input-error">{errors.people?.required}</p>
       <div className="form-line">
-        <button
-          className="submit-button"
-          type="submit"
-          // disabled={}
-        >
+        <button className="submit-button" type="submit" disabled={!isValid}>
           Kaydet
         </button>
       </div>
